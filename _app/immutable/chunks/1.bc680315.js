b01e1a7208b4b565eb67d56c59e17c6a5ae5221b
vfxTurjo <vfxturjo@gmail.com>
2023-03-09 17:00:07 +0600

    Updates

--- _app/immutable/chunks/1.bc680315.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.3fe13de1.js";export{t as component};
--- _app/immutable/chunks/2.c78239ce.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.84afe9ea.js";export{t as component};
